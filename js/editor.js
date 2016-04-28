71a8b50b88569e0aeda1c2a4046c72d640788425
Marvki <marvkipi@gmail.com>
2016-04-27 22:37:59 -0700

    save and delete methods

--- js/editor.js
+++ js/editor.js
@@ -15,6 +15,40 @@ angular.module('App')
 		};
 	}
 
+	$scope.save = function() {
+		$scope.content.date = new Date();
+
+		if($scope.content.id) {
+			$http.put('/notes/' + $scope.content.id, $scope.content).success(function(data) {
+				$scope.editing = false;
+			});
+		} else {
+			$scope.content.id = Date.now();
+			$http.post('/notes/', $scope.content).success(function(data) {
+				$scope.notes.push($scope.content);
+				$scope. editing = false;
+			});
+		}
+	}
+
+	$scope.remove = function() {
+		$http.delete('/notes/' + $scope.content.id).success(function(data) {
+			var found = -1;
+			angular.forEach($scope.notes, function(note, index) {
+				if (note.id === $scope.content.id) {
+					found = index;
+				}
+			});
+			if (found >= 0) {
+				$scope.notes.splice(found, 1);
+			}
+			$scope.content = {
+				title: '',
+				content: ''
+			};
+		});
+	}
+
 	$http.get('/notes').success(function (data) {
 		$scope.notes = data;
 	}).error(function (err) {
